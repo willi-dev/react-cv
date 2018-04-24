21ece6be159e53b7e5bdfb80949f8e2dca93335d
willi-dev <willi.ilmukomputer@gmail.com>
2018-04-24 13:51:36 +0700

    progress update main profile edit form

--- src/components/dashboard/MainProfile.js
+++ src/components/dashboard/MainProfile.js
@@ -16,6 +16,7 @@ const INITIAL_STATE = {
   linkedin: '',
   github: '',
   blog: '',
+  editForm: false,
 }
 
 class MainProfile extends Component {
@@ -23,6 +24,18 @@ class MainProfile extends Component {
   constructor(props){
      super(props);
      this.state = { ...INITIAL_STATE };
+     this.onEditMainProfile = this.onEditMainProfile.bind(this);
+     this.cancelEdit = this.cancelEdit.bind(this);
+  }
+
+  onEditMainProfile(e){
+    e.preventDefault();
+    this.setState({editForm: true})
+  }
+
+  cancelEdit(e){
+    e.preventDefault();
+    this.setState({editForm: false});
   }
 
   componentWillMount() {
@@ -30,7 +43,7 @@ class MainProfile extends Component {
   }
 
   render() {
-    const { name, job_title, phone, address, email, linkedin, github, blog } = this.state;
+    const { name, job_title, phone, address, email, linkedin, github, blog, editForm } = this.state;
     const isInvalid = name === '' || job_title === '' || phone === '' || address === '' || email === '';
     return (
       <div>
@@ -101,7 +114,7 @@ class MainProfile extends Component {
           <div className={this.props.fetched && this.props.main.length > 0 ? 'element-show': 'element-hide'}>
             <div className="form-group">
               <label htmlFor="main-profile-name">Name</label>
-              <input 
+              <input disabled={!editForm}
                 type="text" 
                 className="form-control" 
                 id="main-profile-name" 
@@ -111,43 +124,59 @@ class MainProfile extends Component {
             </div>
             <div className="form-group">
               <label htmlFor="main-profile-job">Job Title</label>
-              <input type="text" className="form-control" id="main-profile-job" placeholder="Example: Front End Developer" value={job_title}
+              <input type="text" disabled={!editForm} className="form-control" id="main-profile-job" placeholder="Example: Front End Developer" value={job_title}
               onChange={event => this.setState(byPropKey('job_title', event.target.value))}/>
             </div>
             <div className="form-group">
               <label htmlFor="main-profile-phone">Phone</label>
-              <input type="text" className="form-control" id="main-profile-phone" placeholder="Example: 0857 2233 8899" value={phone}
+              <input type="text" disabled={!editForm} className="form-control" id="main-profile-phone" placeholder="Example: 0857 2233 8899" value={phone}
               onChange={event => this.setState(byPropKey('phone', event.target.value))}/>
             </div>
             <div className="form-group">
               <label htmlFor="main-profile-address">Address</label>
-              <textarea className="form-control" id="main-profile-address" rows="3" value={address}
+              <textarea className="form-control" disabled={!editForm} id="main-profile-address" rows="3" value={address}
               onChange={event => this.setState(byPropKey('address', event.target.value))}></textarea>
             </div>
             <div className="form-group">
               <label htmlFor="main-profile-email">Email</label>
-              <input type="text" className="form-control" id="mian-profile-email" placeholder="Example: willi.dev.id@gmail.com" value={email}
+              <input type="text" disabled={!editForm} className="form-control" id="mian-profile-email" placeholder="Example: willi.dev.id@gmail.com" value={email}
               onChange={event => this.setState(byPropKey('email', event.target.value))}/>
             </div>
             <div className="form-group">
               <label htmlFor="main-profile-linkedin">Linkedin</label>
-              <input type="text" className="form-control" id="main-profile-linkedin" placeholder="Example: https://www.linkedin.com/in/willidev/" value={linkedin}
+              <input type="text" disabled={!editForm} className="form-control" id="main-profile-linkedin" placeholder="Example: https://www.linkedin.com/in/willidev/" value={linkedin}
               onChange={event => this.setState(byPropKey('linkedin', event.target.value))}/>
             </div>
             <div className="form-group">
               <label htmlFor="main-profile-github">Github</label>
-              <input type="text" className="form-control" id="main-profile-github" placeholder="Example: https://github.com/willi-dev" value={github}
+              <input type="text" disabled={!editForm} className="form-control" id="main-profile-github" placeholder="Example: https://github.com/willi-dev" value={github}
               onChange={event => this.setState(byPropKey('github', event.target.value))}/>
             </div>
             <div className="form-group">
               <label htmlFor="main-profile-blog">Blog</label>
-              <input type="text" className="form-control" id="main-profile-blog" placeholder="Example: https://medium.com/@willi.dev" value={blog}
+              <input type="text" disabled={!editForm} className="form-control" id="main-profile-blog" placeholder="Example: https://medium.com/@willi.dev" value={blog}
               onChange={event => this.setState(byPropKey('blog', event.target.value))}/>
             </div>
             <div className="form-group">
-              <button disabled={isInvalid} className="btn btn-primary" type="submit" >
-                Save Profile
-              </button>
+              {
+                 !editForm && (
+                    <button className="btn btn-primary" type="submit" onClick={this.onEditMainProfile}>
+                      Edit Main Profile
+                    </button>
+                  )
+              }
+              { 
+                editForm && (
+                  <div class="btn-group" role="group" aria-label="Button Edit Form">
+                    <button className="btn btn-success" onClick={this.saveEdit} >
+                      Save Edit
+                    </button>
+                    <button className="btn btn-primary" onClick={this.cancelEdit} >
+                      Cancel
+                    </button>
+                  </div>
+                  )
+              }
             </div>
           </div>
       </div>
