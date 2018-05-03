12c5e8615486d586959ad4220e6391f4ca2e1a3f
willi-dev <willi.ilmukomputer@gmail.com>
2018-05-03 18:21:46 +0700

    remove store/dashboard

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,25 +2,15 @@ import { combineReducers } from 'redux';
 
 import authReducer from './auth/reducer';
 
-import eduReducer from './dashboard/education/reducer';
-import mainReducer from './dashboard/mainprofile/reducer';
-import personalReducer from './dashboard/personaldetail/reducer';
-import publicationReducer from './dashboard/publication/reducer';
-import relatedReducer from './dashboard/related/reducer';
-import skillReducer from './dashboard/skill/reducer';
-import trainingReducer from './dashboard/training/reducer';
-import workReducer from './dashboard/work/reducer';
-import projectReducer from './dashboard/project/reducer';
-
-// import cvEduReducer from './cv/education/reducer';
-// import cvMainReducer from './cv/mainprofile/reducer';
-// import cvPersonalReducer from './cv/personaldetail/reducer';
-// import cvPublicationReducer from './cv/publication/reducer';
-// import cvRelatedReducer from './cv/related/reducer';
-// import cvSkillReducer from './cv/skill/reducer';
-// import cvTrainingReducer from './cv/training/reducer';
-// import cvWorkReducer from './cv/work/reducer';
-// import cvProjectReducer from './cv/project/reducer';
+import eduReducer from './cv/education/reducer';
+import mainReducer from './cv/mainprofile/reducer';
+import personalReducer from './cv/personaldetail/reducer';
+import publicationReducer from './cv/publication/reducer';
+import relatedReducer from './cv/related/reducer';
+import skillReducer from './cv/skill/reducer';
+import trainingReducer from './cv/training/reducer';
+import workReducer from './cv/work/reducer';
+import projectReducer from './cv/project/reducer';
 
 export default combineReducers({
 	auth: authReducer,
