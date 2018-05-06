188e79483826bc780306cfc92ea03ca750ec81a2
willi-dev <willi.ilmukomputer@gmail.com>
2018-05-06 23:03:10 +0700

    remove redux logger when in production env

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
 // import promise from 'redux-promise-middleware';
 import thunk from 'redux-thunk';
-
 import reducers from './reducers';
-
-const middleware = applyMiddleware( thunk, logger );
+import logger from 'redux-logger';	
+if( process.env.NODE_ENV === 'development' ){
+	const middleware = applyMiddleware( thunk, logger );
+}
+const middleware = applyMiddleware( thunk );
 
 export default createStore( reducers, middleware );
