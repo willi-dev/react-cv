0cd581c69a31d97346acf97a8f739c10171e333a
willi-dev <willi.ilmukomputer@gmail.com>
2018-05-22 12:05:14 +0700

    disable reverse data in dashboard project

--- src/store/store.js
+++ src/store/store.js
@@ -3,9 +3,9 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 import logger from 'redux-logger';	
-if( process.env.NODE_ENV === 'development' ){
-	const middleware = applyMiddleware( thunk, logger );
-}
+// if( process.env.NODE_ENV === 'development' ){
+// 	const middleware = applyMiddleware( thunk, logger );
+// }
 const middleware = applyMiddleware( thunk );
 
 export default createStore( reducers, middleware );
